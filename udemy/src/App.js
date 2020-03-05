279ab5149c78823f84e200a744c996e296078cfb
akbel <colakakbel@gmail.com>
2020-03-05 22:37:02 +0100

    color change onclick

--- udemy/src/App.js
+++ udemy/src/App.js
@@ -3,11 +3,34 @@ import React, {useState} from 'react';
 
 function App() {
   const [count,setCount] = useState(0)
+  const [isOn,setOn] = useState(false)
+
+  const toggleLight = ()=>{
+    //setOn( isOn===false ? style= {bacground:'grey'} : style={bacground:'green'} )
+
+    setOn(prevIsOn => !prevIsOn)
+  }
 
   return(
-    <div>
-      <button onClick={()=>setCount(count+1)}>I was clicked {count} times</button>
-    </div>
+    <>
+      <h2>counter</h2>
+      <button onClick={()=>setCount(prevCount => prevCount+1)}>I was clicked {count} times</button>
+      
+      <h2> toggle light</h2>
+      <img
+        alt='FlashLight'
+        src={
+          isOn ? 'https:icon.now.sh/highlight/fd0' : 'https:icon.now.sh/highlight/aaa'
+        }
+        style={{height:50 ,
+        width:50
+        }}
+        onClick={toggleLight}
+      />
+
+      
+
+    </>
   )
 }
 
