155604332809fd3561eb2cd27a2c4cff8a62247d
akbel <colakakbel@gmail.com>
2020-03-05 23:23:51 +0100

    useEffect and comments

--- udemy/src/App.js
+++ udemy/src/App.js
@@ -1,13 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
   const [isOn, setOn] = useState(false);
+  const [mousePozition, setMousePozition] = useState({ x:null, y:null})
+
+  useEffect( ()=> {
+    //websayfasinin basligini degistirmek icin
+    // bu Effecti sonrasinda silmemize gerek yok cunku sadece click ile calisiyor
+    document.title = `${count} times clicked`;
+    // mouse hareketlerinin sayfadaki yerini gormek icin
+    window.addEventListener('mousemove', handleMouseMove)
+
+    //componentWillUnmount- mouse'un x ve y pozisyonunu gostermesi bittiginde eventi temizlemek icin
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove)
+    }
+  }, [count])
+  /* sayfayi her actiginda componentin temizlenmesini istiyorsan sondaki [] koyma
+  aralikli olarak temizlemesini istiyorsan [] bu sekilde koy
+  count ile ilgili olan effecti etkilemesini istemiyorsan [count] yaz
+  */
+ 
+  const handleMouseMove = (event)=>{
+    setMousePozition( {
+      x: event.pageX,
+      y: event.pageY
+    })
+  }
 
   const toggleLight = () => {
-    //setOn( isOn===false ? style= {bacground:'grey'} : style={bacground:'green'} )
-    // arkaplani buradan degil img'in icine background=buFonksiyon yazarak degistirebilirsin
-
     setOn(prevIsOn => !prevIsOn);
   };
 
@@ -29,6 +51,10 @@ function App() {
         style={{ height: 50, width: 50 }}
         onClick={toggleLight}
       />
+
+      <h2> mouse position </h2>
+      {JSON.stringify(mousePozition, null, 2)}
+      
     </>
   );
 }
